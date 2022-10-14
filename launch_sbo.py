8c7f0bd905e0d78996dab157c621c7cc0a53fd29
lukeraphael <luke.r.chow@gmail.com>
2022-10-14 10:59:51 +0800

    feat: experiment runner

--- launch_sbo.py
+++ launch_sbo.py
@@ -5,11 +5,11 @@ import os
 # generations, workers, population size
 params = [
     # [1,1,10],
-    # [20, 10, 50],
-    # [40, 10, 50],
-    # [60, 10, 50],
-    # [80, 10, 50],
-    # [100, 10, 50],
+    # [20, 10, 100],
+    # [40, 10, 100],
+    # [60, 10, 100], 
+    # [80, 10, 100],
+    # [100, 10, 100],
     # [120, 10, 50],
     # [140, 10, 50],
     # [160, 10, 50],
@@ -21,27 +21,45 @@ params = [
     # [50, 10, 120],
     # [50, 10, 140],
     # [50, 10, 160],
-    [5, 10, 50],
-    [5, 20, 50],
-    # [5, 30, 50],
-    # [5, 40, 50],
-    # [5, 50, 50]
-]
+    # server vs standalone
+    # [10, 20, 20],
+    # [20, 20, 20],
+    # [30, 20, 20],
+    # [40, 20, 20],
+    [50, 20, 20],
+    ]    
+
 
 config.load_kube_config()
 v1 = client.CoreV1Api()
 namespace = "argo"
+server_standalone = "standalone"
+image = "lukeraphael/sbo"
+app_name = "sbo"
+output_path = "experiments/argo_standalone_kubernetes_output.csv"
+token = "Bearer 2764ff3c24659a11cf79a3fb128221abcf11ff4f2ab0a58620793bc27689d7513d9dd12ee3e5f49f836a52ff3c3c1197b6d5001c7434222f300ad091986d2b05"
 
 for gen, workers, pop_size in params:
     command = ["python3", "parallelisation.py", 
         "--workers", str(workers), 
         "--n_gen", str(gen), 
         "--pop_size", str(pop_size), 
-        "--choice", "kubernetes"
+        "--choice", "argo",
+        "--token", token,
     ]
+    if server_standalone == "server":
+        image = "lukeraphael/sbo:server"
+        command[1] = "server_parallelisation.py"
+        command.append("--host")
+        command.append("10.0.22.243")
+        command.append("--port")
+        command.append("5001")
+        app_name = "sbo-server"
+        output_path = "experiments/server_kubernetes_output.csv"
+
     pod = deploy.create_pod_object(
-        "sbo", 
-        "lukeraphael/sbo:latest", 
+        app_name, 
+        image, 
         command, 
         "argo-pv-volume", 
         "argo-pv-claim", 
@@ -62,9 +80,9 @@ for gen, workers, pop_size in params:
     print(logs, workers, gen, pop_size)
     
     # save into csv
-    with open("experiments/standalone_kubernetes_output.csv", "a") as f:
+    with open(output_path, "a") as f:
         # if not empty, insert newline
-        if os.stat("experiments/standalone_kubernetes_output.csv").st_size != 0:
+        if os.stat(output_path).st_size != 0:
             f.write("\n")
         f.write(f"{workers},{gen},{pop_size},{logs}")
 
